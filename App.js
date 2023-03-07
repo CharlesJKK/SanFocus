d3a6cda7e1256c7617223c8ae98f75f4b8ed8c57
CharlesJKK <selrahcjunior@hotmail.com>
2023-03-07 20:01:52 -0300

    feat: ínicio com toda a parte gráfica

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {Image, View, TouchableOpacity, Text} from 'react-native';
 
 export default function App(){
   
   return(
-    <View style={{}}>
-      <Text>primeiro texto</Text>
+    <View style={{flex: 1, alignItems: 'center', backgroundColor: '#642d8a'}}>
+      <Image source={require('./assets/logoimg.png')} style={{ width: 250, height: 250 }} resizeMode="cover"/>
+      <View style={{backgroundColor: '#214C9B', width: '40%', height: '20%', borderRadius: 100, justifyContent: 'center', alignItems: 'center'}}>
+        <Text style={{fontSize: 30, fontWeight: 700}}>25:00</Text>
+      </View>
+      <TouchableOpacity style={{backgroundColor: '#6495ED', borderRadius: 5, marginTop: 40, height: 40, width: 80, justifyContent: 'center', alignItems: 'center'}}>
+        <Text style={{color: '#214C9B', fontWeight: 700}}>Iniciar</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={{backgroundColor: '#6495ED', borderRadius: 5, marginTop: 20, height: 40, width: 80, justifyContent: 'center', alignItems: 'center'}}>
+        <Text style={{color: '#214C9B', fontWeight: 700}}>Parar</Text>
+      </TouchableOpacity>
     </View>
   )
 }
