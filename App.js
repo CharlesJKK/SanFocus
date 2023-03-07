6722923b35d41cf611f9dacdee5044afbc422a8c
CharlesJKK <selrahcjunior@hotmail.com>
2023-03-07 19:57:02 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default function App(){
+  
+  return(
+    <View style={{}}>
+      <Text>primeiro texto</Text>
+    </View>
+  )
+}
+
+
