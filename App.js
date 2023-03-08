61833c5a89289f2330d965007da70612ff5f5fa7
CharlesJKK <selrahcjunior@hotmail.com>
2023-03-08 00:06:17 -0300

    feat: lógica do timer completa com estilos quase completos

--- App.js
+++ App.js
@@ -3,19 +3,37 @@ import {Image, View, TouchableOpacity, Text} from 'react-native';
 
 export default function App(){
 
-  const timeMinutes = 0.2 * 60 * 1000;
+  const focusTime = 0.2 * 60 * 1000;
+  const breakTime = 0.1 * 60 * 1000;
 
-  const [timer, setTimer] = useState(timeMinutes)
+  const [timer, setTimer] = useState(focusTime)
+  const [handleStart, setHandleStart] = useState(false)
+  const [timerMode, setTimerMode] = useState('Focus')
   const [isRunning, setIsRunning] = useState(null)
+
+
+  useEffect(() => {
+    if(timer === 0){
+      if(timerMode === 'Focus'){
+        setTimerMode('Break')
+        setTimer(breakTime)
+      }else{
+        setTimerMode('Focus')
+        setTimer(focusTime)
+      }
+      timerStop()
+    }
+  }, [timer])
 
   function timerStart(){
     const res = setInterval(() => setTimer(prev => prev - 1000), 1000)
     setIsRunning(res)
-    console.log(timeMinutes)
+    setHandleStart(true)
   }
 
   function timerStop(){
     clearInterval(isRunning)
+    setHandleStart(false)
   }
 
   const timerDate = new Date(timer)
@@ -23,32 +41,16 @@ export default function App(){
   return(
     <View style={{flex: 1, alignItems: 'center', backgroundColor: '#642d8a'}}>
       <Image source={require('./assets/logoimg.png')} style={{ width: 250, height: 250 }} resizeMode="cover"/>
-      <View style={{backgroundColor: '#214C9B', width: '40%', height: '20%', borderRadius: 100, justifyContent: 'center', alignItems: 'center'}}>
+      <Text style={{fontSize: 30, textAlign: 'center', color: timerMode === 'Focus' ? '#cb0e00' : '#49b8a5'}}>{timerMode === 'Focus' ? 'Hora de focar!!!' : 'Hora do descanso!!!'}</Text>
+      <View style={{backgroundColor: timerMode === 'Focus' ? '#cb0e00' : '#49b8a5', width: '40%', height: '20%', borderRadius: 100, justifyContent: 'center', alignItems: 'center'}}>
         <Text style={{fontSize: 30, fontWeight: 700}}>{timerDate.getMinutes().toString().padStart(2, "0")}:{timerDate.getSeconds().toString().padStart(2, "0")}</Text>
       </View>
-      <TouchableOpacity onPress={timerStart} style={{backgroundColor: '#6495ED', borderRadius: 5, marginTop: 40, height: 40, width: 80, justifyContent: 'center', alignItems: 'center'}}>
+      <TouchableOpacity disabled={handleStart} onPress={timerStart} style={{backgroundColor: '#6495ED', borderRadius: 5, marginTop: 40, height: 40, width: 150, justifyContent: 'center', alignItems: 'center'}}>
         <Text style={{color: '#214C9B', fontWeight: 700}}>Iniciar</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={timerStop} style={{backgroundColor: '#6495ED', borderRadius: 5, marginTop: 20, height: 40, width: 80, justifyContent: 'center', alignItems: 'center'}}>
+      <TouchableOpacity onPress={timerStop} style={{backgroundColor: '#6495ED', borderRadius: 5, marginTop: 20, height: 40, width: 150, justifyContent: 'center', alignItems: 'center'}}>
         <Text style={{color: '#214C9B', fontWeight: 700}}>Parar</Text>
       </TouchableOpacity>
     </View>
   )
 }
-
-
-/*
-
-
-  function timerReset(){
-    clearInterval(timerInterval)
-    setUserMinutes(0.2)
-  }
-
-      <TouchableOpacity onPress={timerReset} style={{backgroundColor: '#6495ED', borderRadius: 5, marginTop: 20, height: 40, width: 80, justifyContent: 'center', alignItems: 'center'}}>
-        <Text style={{color: '#214C9B', fontWeight: 700}}>Resetar</Text>
-      </TouchableOpacity>
-
-      */
-
-
