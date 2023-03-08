c47a8cc5ca60744c6f34dd97ce24244feff09fff
CharlesJKK <selrahcjunior@hotmail.com>
2023-03-08 14:40:03 -0300

    feat: funcionalidades completas, faltando apenas deixar o código limpo

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import {Image, View, TouchableOpacity, Text, TextInput, KeyboardAvoidingView} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 var Sound = require('react-native-sound');
 import alertSong from './assets/endAlert.mp3'
 
@@ -17,7 +18,27 @@ export default function App(){
   const [handleStart, setHandleStart] = useState(false)
   const [timerMode, setTimerMode] = useState('Focus')
   const [isRunning, setIsRunning] = useState(null)
+
   const [pomodoroCount, setPomodoroCount] = useState(0)
+
+  const storeData = async (value) => {
+    try {
+      await AsyncStorage.setItem('@pomodoro_countDay', value)
+    } catch (e) {
+        console.log(e)
+    }
+  }
+
+  const getCounts = async () => {
+    try{
+      const value = await AsyncStorage.getItem('@pomodoro_countDay')
+      if(value !== null){ 
+        setPomodoroCount(Number(value))
+      }
+    }catch(e){
+        console.log(e)
+    }
+  }
 
   Sound.setCategory('Alarm');
 
@@ -29,15 +50,16 @@ export default function App(){
   });
 
   useEffect(() => {
+    getCounts()
     if(timer === 0){
 
       setTimeout(() => {
         sound.setVolume(0.1)
         sound.play()
-      }, 50)
+      }, 100)
 
       if(timerMode === 'Focus'){
-        setPomodoroCount(pomodoroCount + 1)
+        storeData((pomodoroCount + 1).toString())
         setTimerMode('Break')
         setTimer(breakTime)
       }else{
@@ -47,7 +69,7 @@ export default function App(){
       }
       timerStop()
     }
-  }, [timer, focusMinutes, breakMinutes])
+  }, [timer, focusMinutes])
 
   function timerStart(){
     const res = setInterval(() => setTimer(prev => prev - 1000), 1000)
@@ -62,12 +84,14 @@ export default function App(){
 
   function timerReset(){
     timerStop()
+    setFocusMinutes(25)
+    setbreakMinutes(5)
     setTimer(focusTime)
     setTimerMode('Focus')
   }
 
   function applyChanges(){
-      if(focusMinutes === ''){
+      if(focusMinutes === '' || focusMinutes < 0.1){
         return
       }else{
         timerStop()
@@ -81,7 +105,7 @@ export default function App(){
   return(
     <KeyboardAvoidingView style={{flex: 1, alignItems: 'center', backgroundColor: '#642d8a', justifyContent: 'center', width: '100%', height: '100%'}} behavior= 'position' keyboardVerticalOffset={28}>
       <Image source={require('./assets/logoimg.png')} style={{ width: 250, height: 250, alignSelf: 'center' }} resizeMode="cover"/>
-      {pomodoroCount >= 4 ? <Text style={{alignSelf: 'center'}}>Você já fez 4 sessões, hora de descansar</Text> : <></>}
+      {pomodoroCount == 4 ? <Text style={{alignSelf: 'center', position: 'absolute'}}>Você já fez 4 sessões, hora de descansar pelo menos {breakMinutes * 2}m</Text> : <></>}
       <Text style={{fontSize: 30, textAlign: 'center', color: timerMode === 'Focus' ? '#cb0e00' : '#49b8a5'}}>{timerMode === 'Focus' ? 'Hora de focar!!!' : 'Hora do descanso!!!'}</Text>
       <View style={{backgroundColor: timerMode === 'Focus' ? '#cb0e00' : '#49b8a5', width: 150, height: '20%', borderRadius: 100, justifyContent: 'center', alignItems: 'center', alignSelf: 'center'}}>
         <Text style={{fontSize: 30, fontWeight: 700}}>{timerDate.getMinutes().toString().padStart(2, "0")}:{timerDate.getSeconds().toString().padStart(2, "0")}</Text>
@@ -109,7 +133,7 @@ export default function App(){
       <TouchableOpacity onPress={timerReset} style={{backgroundColor: '#6495ED', alignSelf: 'center', borderRadius: 5, marginTop: 20, height: 40, width: 150, justifyContent: 'center', alignItems: 'center'}}>
         <Text style={{color: '#214C9B', fontWeight: 700}}>Resetar</Text>
       </TouchableOpacity>
-      <Text style={{alignSelf: 'center', paddingTop: 10, fontSize: 18}}>Quantidade de sessões concluídas: {pomodoroCount}</Text>
+      <Text style={{alignSelf: 'center', paddingTop: 10, fontSize: 18}}>Quantidade de sessões concluídas: {pomodoroCount.toString()}</Text>
     </KeyboardAvoidingView>
   )
 }
